53bce9f99e7deaf9abe34e02c318ac7a9a982d65
wasieDev <127023454+rens20@users.noreply.github.com>
2023-12-09 20:39:39 +0800

    Add files via upload

---
+++ christmass tree.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << endl;
+    cout << endl;
+    cout << endl;
+
+    cout << "                                  [+]" << endl;
+    cout << "                                [][][]" << endl;
+    cout << "                              [][][][][]                          "<< name << " happy holiday" << endl;
+    cout << "                         [][][][][][][][][][][]                  "  << "Wishing you a merry Christmas" << endl;
+    cout << "                      [][][][][][][][][][][][][][]                " << "and a happy new year!" << endl;
+    cout << "                              [][][][[]" << endl;
+    cout << "                                [][][]" << endl;
+    cout << "                            [][][][][][][][]" << endl;
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "                                                                    from C++ coding" << endl;
+    cout << "                                                                        Rens Belga Acuna" << endl;
+    cout << "                                                                     ICT 11 4 Batch 2 PC No." << endl;
+
+    return 0;
+}
